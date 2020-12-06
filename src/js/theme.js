71c0a8c56d4a33848f43356d29d6440181188939
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2020-12-06 02:21:28 +0200

    Update theme.js

--- src/js/theme.js
+++ src/js/theme.js
@@ -10,11 +10,25 @@ const refs = {
 
 refs.toggleSwitch.addEventListener('change', e => {
     if (e.target.checked) {
-        refs.body.classList.add(Theme.DARK);
+        refs.body.classList.toggle(Theme.DARK);
         localStorage.setItem('theme', 'dark-theme');
     }
     else {
-        refs.body.classList.add(Theme.LIGHT);
+        refs.body.classList.toggle(Theme.LIGHT);
         localStorage.setItem('theme', 'light-theme');
     }
 });
+
+function switchStatus() {
+    const status = localStorage.getItem('theme');
+    if (status === 'dark-theme') {
+        refs.body.classList.add(Theme.DARK);
+        refs.toggleSwitch.checked = true;
+    }
+
+    if (status === 'light-theme') {
+        refs.body.classList.add(Theme.LIGHT);
+        refs.toggleSwitch.checked = false;
+    }
+};
+switchStatus();
